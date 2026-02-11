df06fd61ea4d2aa84fe17a16770ea6e1c5597c4b
Arunima Das (Devon) <arunima.das@devon.nl>
2026-02-11 11:38:02 +0530

    dockerfile update

---
+++ terraform/github-oidc.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role" "github_oidc_role" {
+  name = "github-oidc-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_oidc_policy" {
+  name = "github-oidc-policy"
+  role = aws_iam_role.github_oidc_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::tf-state-ad/*"
+        ]
+      }
+    ]
+  })
+}
+
